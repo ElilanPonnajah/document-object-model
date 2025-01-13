e03e8fc200bfb19f6cb412846c5982a1e6597524
ElilanPonnajah <302905573@student.rocmondriaan.nl>
2025-01-13 10:28:28 +0100

    bijna af

--- js/main.js
+++ js/main.js
@@ -13,3 +13,31 @@ function addPlanet(){
     const planetsView = document.querySelector(".plannitz");
     planetsView.innerHTML += "<li>" + planet + "</li>";
 }
+
+
+const h1Name = document.querySelector('.h1');
+h1Name.textContent = "BOM"; 
+
+
+const pSubtitle = document.querySelector('p');
+pSubtitle.textContent = "50 euro"; 
+
+const Olist = document.querySelector('.list');
+Olist.innerHTML = ""; 
+
+for (let i = 0; i < planets.length; i++) {
+    const planet = planets[i];
+    Olist.innerHTML += `<li> ${planet} </li> `;
+}
+
+
+    
+const navigation = document.querySelector(".navigation");
+navigation.innerHTML = routes; 
+
+
+
+const addBtn = document.querySelector("btn-add-planet");
+htmlElement.addEventListener("click",)
+
+addPlanet();
