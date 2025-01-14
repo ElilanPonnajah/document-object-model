4c9be9f4aef9a2f26a3c30a82e40e84cf98462f8
ElilanPonnajah <302905573@student.rocmondriaan.nl>
2025-01-14 10:59:31 +0100

    opdrachten

---
+++ DOM opdracht/js/main.js
@@ -0,0 +1,50 @@
+const title = "Planeten";
+const subTitle = "Alles over planeten";
+const planets = ["Mercurius", "Venus", "Aarde", "Mars", "Jupiter", "Saturnus", "Uranus", "Neptunus"];
+const routes = ["Home", "Universum", "Planeten"];
+
+
+
+
+
+function addPlanet(){
+    const planet = prompt("Welke planeet wil je toevoegen?");
+    planets.push(planet);
+    const planetsView = document.querySelector(".plannitz");
+    planetsView.innerHTML += "<li>" + planet + "</li>";
+}
+
+
+const h1Name = document.querySelector('.h1');
+h1Name.textContent = "BOM"; 
+
+
+const pSubtitle = document.querySelector('p');
+pSubtitle.textContent = "50 euro"; 
+
+const Olist = document.querySelector('.list');
+Olist.innerHTML = ""; 
+
+for (let i = 0; i < planets.length; i++) {
+    const planet = planets[i];
+    Olist.innerHTML += `<li> ${planet} </li> `;
+}
+
+
+    
+const navigation = document.querySelector(".navigation");
+navigation.innerHTML = routes; 
+
+
+
+// const addBtn = document.querySelector(".btn-add-planet");
+
+// addBtn.addEventListener("click", addPlanet()){
+//     Olist.innerHTML += `<li> ${planet} </li> `;
+// }; 
+
+const makeDark = document.querySelector(".btn btn-make-dark"); 
+makeDark.addEventListener("click", makeDark);
+
+
+
---
+++ js opdrachten werk/main.js
@@ -0,0 +1,32 @@
+const favMovies = ["rush hour", "extraction", "barbie", "sonic", "coach carter"];
+ for(let i = 0; i < favMovies.length; i++){
+    console.log(favMovies[i]);
+}
+
+
+const users = []; 
+
+
+
+
+const result = prompt("voeg  gebruiker toe");
+const result2 = prompt("voeg  gebruiker 2 toe");
+const result3 = prompt("voeg  gebruiker 3 toe");
+const results = result + " " +  result2 +  " " + result3; 
+users.push(results)
+
+while (users < 3) {
+    console.log(users); 
+}
+ 
+    
+
+
+// let numbers = [10, 15, 20, 25, 30]; 
+// let total = 0; 
+
+// for (let i = 0; i < numbers.length; i++){
+//     total += numbers[i]
+    
+// }
+// console.log(numbers[i]);    
